c6fab21fd9f1340c23432abacd15ba2717b6d290
Mustafa Alshammaa <adam.benals@gmail.com>
2020-08-21 13:55:14 +0200

    Initial commit, the quote generator project

---
+++ script.js
@@ -0,0 +1,64 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const quoteIcon = document.getElementById('quote-icon');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+  }
+}
+
+async function getQuoteFromApi() {
+  showLoadingSpinner();
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    // If Author is blank, add 'Unknown'
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
+    // Stop Loader, Show Quote
+    removeLoadingSpinner();
+  } catch (error) {
+    removeLoadingSpinner();
+    quoteIcon.style.display = 'none';
+    quoteText.innerText = 'Oops, no quote. Please try again!';
+    authorText.innerText = 'www';
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuoteFromApi);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuoteFromApi();
